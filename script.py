5e643bbffaac4ca44437c54c747f8647b29345cb
ivanvinokurov23 <fizelab@yandex.ru>
2026-01-18 02:27:04 +0300

    added comments

--- script.py
+++ script.py
@@ -1,6 +1,8 @@
 def top_k_words(text: str, k: int):
+    # добавляем небуквенный символ в конец для корректной обработки последнего слова
     text = (text + '.').lower()
     words_dict = dict()
+    # флаг на новое слово
     new_word = True
     curr_word = ''
     for char in text:
@@ -17,6 +19,7 @@ def top_k_words(text: str, k: int):
                     words_dict[curr_word] = 1
             new_word = True
             curr_word = ''
+    # непосредственная сортировка по условию задачи
     sorted_dict = sorted(words_dict.items(), key=lambda item: (-item[1], item[0]))
     return sorted_dict[0:k]
 
